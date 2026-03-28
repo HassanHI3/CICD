6274f0600b4ec059f7cdcd51931519804042f91d
HassanHI3 <Hassanhi3@icloud.com>
2026-03-28 21:12:16 +0000

    rebuild custom action with license file

--- custom-one/index.js
+++ custom-one/index.js
@@ -1,11 +1,10 @@
-const core = require('@/Users/hassanibrahim/CICD/custom-one/node_modules/@actions/core');
+const core = require('@actions/core');
 
 try {
-  // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
     console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const time = new Date().toTimeString();
+    core.setOutput('time', time);
 }   catch (error) {
     core.setFailed(error.message);
 }
